a49d6dcb5792dbfd33cef67ea8c6356215a2f34e
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-02-16 12:44:14 -0500

    use conditionals to tell ppl about the weather

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+
+
+const temperature = 19;
+
+if (temperature <0) {
+  console.log("Make sure you pick a scarf");
+} else if (temperature <15) {
+  console.log("shorts wont cut it");
+} else {
+  console.log("short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside");
